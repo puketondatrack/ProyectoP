8cd5fe71efa29f6b42e774c719785a067827ede0
Kevin Rivera Ramirez <kevin14109836@gmail.com>
2022-12-09 00:25:29 -0600

    login - logout

--- app/src/main/java/com/example/proyectop/Principal.kt
+++ app/src/main/java/com/example/proyectop/Principal.kt
@@ -2,6 +2,7 @@ package com.example.proyectop
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,6 +13,8 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyectop.databinding.ActivityPrincipalBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Principal : AppCompatActivity() {
 
@@ -53,5 +56,15 @@ class Principal : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_principal)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_logout ->{
+                Firebase.auth.signOut()
+                finish()
+                true
+            }else -> super.onOptionsItemSelected(item)
+        }
     }
 }
